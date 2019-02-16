823ce6a6409d328db0d369aafdbd838cb63487ba
hazem osama ibrahim <hazem3500@gmail.com>
2019-02-16 17:44:11 +0200

    added card and grid component

---
+++ src/components/Card.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <img :src="cat.url">
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Card",
+  props: {
+    cat: Object
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ src/components/Grid.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="grid">
+    <infinite-loading>
+      <Card v-for="cat in cats" :cat="cat" :key="cat.id"/>
+    </infinite-loading>
+  </div>
+</template>
+
+<script>
+import Card from "@/components/Card.vue";
+
+export default {
+  name: "Grid",
+  components: {
+    Card
+  },
+  props: {
+    cats: Array
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.grid {
+}
+</style>
+
