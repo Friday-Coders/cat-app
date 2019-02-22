38c779586931633e18c57f3fae34897df2ba9e94
hazem osama ibrahim <hazem3500@gmail.com>
2019-02-22 17:38:58 +0200

    added filter component

---
+++ src/components/Loading.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div>Loading</div>
+    <p>{{fact}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Loading",
+  data: function() {
+    return {
+      fact: localStorage.getItem("fact") || "blah blah"
+    };
+  },
+  created: function() {
+    this.getFact();
+  },
+  methods: {
+    getFact: function() {
+      console.log("fetched cat fact");
+    }
+  }
+};
+</script>
