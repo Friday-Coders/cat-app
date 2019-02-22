7e09e0083a8a3a447518ee38b89a5404e4e0b6c3
El-Hussein Abdelraouf <hussein@raoufs.me>
2019-02-22 19:01:56 +0200

    impelemented upload image

--- src/views/Upload.vue
+++ src/views/Upload.vue
@@ -1,18 +1,21 @@
 <template>
   <div>
-    <input type="file" name="cat-photo" @change="addPhoto">
-    <img :src="catPhoto">
+    <input type="file" name="cat-photo" @change="addPhoto" />
+    <img :src="catPhoto" />
     <button @click="uploadPhoto">Upload</button>
     <button @click="removePhoto">Remove</button>
   </div>
 </template>
 
 <script>
+import CatApiService from "../services/CatApiService.js";
+
 export default {
   name: "Upload",
   data: function() {
     return {
       catPhoto: null,
+      catFile: null,
       dropzoneOptions: {
         dictDefaultMessage: "Upload Cat Image"
       }
@@ -22,17 +25,18 @@ export default {
     addPhoto: function(e) {
       var files = e.target.files || e.dataTransfer.files;
       if (!files.length) return;
-      var image = new Image();
-      var reader = new FileReader();
       var vm = this;
+      vm.catFile = files[0];
 
+      var reader = new FileReader();
       reader.onload = e => {
         vm.catPhoto = e.target.result;
       };
       reader.readAsDataURL(files[0]);
     },
-    uploadPhoto: function() {
-      console.log("uploaded photo");
+    uploadPhoto: async function() {
+      const response = await CatApiService.uploadImage(this.catFile);
+      console.log("uploaded photo", response);
     },
     removePhoto: function() {
       this.catPhoto = "";
