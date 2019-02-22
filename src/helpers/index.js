9d4ecda05654849267e3338cd0adc1acff521274
El-Hussein Abdelraouf <hussein@raoufs.me>
2019-02-22 18:48:20 +0200

    fix issue in jsonToQueryString helper

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,4 +1,4 @@
-export const jsonToQueryString = json => {
+export const jsonToQueryString = (json = "") => {
   return Object.keys(json).reduce(
     (acc, key) => acc + `${key}=${json[key]}&`,
     ""
