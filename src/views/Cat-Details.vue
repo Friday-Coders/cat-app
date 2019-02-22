c8baf54943511b83639d1b25a0b07285b65d1581
El-Hussein Abdelraouf <hussein@raoufs.me>
2019-02-23 01:40:21 +0200

    add delete and write on image buttons

--- src/views/Cat-Details.vue
+++ src/views/Cat-Details.vue
@@ -2,6 +2,8 @@
   <div>
     <button @click="deepDream">Deep Dream</button>
     <button>COOL 🕶</button>
+    <button v-if="this.isMine" @click="deleteCat">Delete</button>
+    <button @click="writeOnImage">Write On Image</button>
     <div>
       <img :src="image" />
     </div>
@@ -11,17 +13,20 @@
 <script>
 import CatApiService from "../services/CatApiService.js";
 import DeepAIService from "../services/DeepAIService.js";
+import MemeGeneratorService from "../services/MemeGeneratorService.js";
 
 export default {
   name: "Cat-Details",
   data: function() {
     return {
       id: this.$route.params.catId,
-      image: null
+      image: null,
+      isMine: false
     };
   },
   created: function() {
     this.getCatDetails();
+    this.checkIfMine();
   },
   methods: {
     getCatDetails: async function() {
@@ -29,11 +34,26 @@ export default {
       this.image = returnedCat.url;
       console.log("fetched cat data", returnedCat);
     },
+    checkIfMine: async function() {
+      const mycats = await CatApiService.getMyImages();
+      this.isMine = mycats.some(cat => cat.id == this.id);
+    },
     deepDream: async function() {
-      console.log("Qwe");
-
       const response = await DeepAIService.deepDream(this.image);
       this.image = response.output_url;
+    },
+    deleteCat: async function() {
+      await CatApiService.deleteImage(this.id);
+      this.$router.push("/");
+    },
+    writeOnImage: function() {
+      const response = MemeGeneratorService.generateMemeFromImageUrl(
+        this.image,
+        "Hello",
+        "Hello World"
+      );
+
+      this.image = response;
     }
   }
 };
