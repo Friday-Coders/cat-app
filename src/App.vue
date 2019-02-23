edbb21352f0181447e213d575463937eea74fa47
hazem osama ibrahim <hazem3500@gmail.com>
2019-02-23 08:23:31 +0200

    updated loading component

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,37 @@
 <template>
   <div id="app">
-    <top-nav />
-    <router-view />
+    <top-nav/>
+    <router-view @loading:on="turnOnLoading" @loading:off="turnOffLoading"/>
+    <Loading v-show="isLoading" :isLoading="isLoading"/>
   </div>
 </template>
 
 <script>
 import TopNav from "@/components/TopNav.vue";
+import Loading from "@/components/Loading.vue";
 
 export default {
   components: {
-    TopNav
+    TopNav,
+    Loading
+  },
+  data: function() {
+    return {
+      isLoading: false,
+      loadingTimeOut: null
+    };
+  },
+  methods: {
+    turnOnLoading: function() {
+      if (this.loadingTimeOut) clearTimeout(this.loadingTimeOut);
+      this.loadingTimeOut = setTimeout(() => {
+        this.isLoading = true;
+      }, 100);
+    },
+    turnOffLoading: function() {
+      if (this.loadingTimeOut) clearTimeout(this.loadingTimeOut);
+      this.isLoading = false;
+    }
   }
 };
 </script>
